0ca80ddb856d25d523321a72676e91100b3fc68d
thenameiswiiwin <huyn.nguyen95@gmail.com>
2023-07-20 22:29:22 -0500

    feat(journal, auth.ts): fetch all entries from in database. Associate clerkId with current user

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,4 +1,23 @@
-const JournalPage = () => {
+import { getUserByClerkID } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+
+const getEntries = async () => {
+  const user = await getUserByClerkID()
+  const entries = await prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+
+  return entries
+}
+
+const JournalPage = async () => {
+  const entries = await getEntries()
+  console.log('entries', entries)
   return <div>journal</div>
 }
 
