d79fe61d6befd138b31108e500b3af1bf4d9190b
thenameiswiiwin <huyn.nguyen95@gmail.com>
2023-07-25 16:43:38 -0500

    feat(journal/[id]): Added HTML structure and basic styling

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -24,10 +24,37 @@ interface EntryPageProp {
 
 const EntryPage = async ({ params }: EntryPageProp) => {
   const entry = await getEntry(params.id)
+  const analysisData = [
+    { name: 'Summary', value: '' },
+    { name: 'Subject', value: '' },
+    { name: 'Mood', value: '' },
+    { name: 'Negative', value: 'False' },
+  ]
+
   if (entry) {
     return (
-      <div className="h-full w-full">
-        <Editor entry={entry} />
+      <div className="grid h-full w-full grid-cols-3">
+        <div className="col-span-2">
+          <Editor entry={entry} />
+        </div>
+        <div className="border-l border-black/10">
+          <div className="bg-blue-300 px-6 py-10">
+            <h2 className="text-2xl">Analysis</h2>
+          </div>
+          <div>
+            <ul>
+              {analysisData.map((item) => (
+                <li
+                  key={item.name}
+                  className="flex items-center justify-between border-y border-black/10 px-2 py-4"
+                >
+                  <span className="text-lg font-semibold">{item.name}</span>
+                  <span>{item.value}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     )
   }
