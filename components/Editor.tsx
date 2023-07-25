8841707656994bba7ef165621a6d29827861f0aa
thenameiswiiwin <huyn.nguyen95@gmail.com>
2023-07-25 15:59:18 -0500

    feat(Editor.tsx): autosave on edited entry every 2 seconds

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,16 +1,31 @@
 'use client'
 
 import { useState } from 'react'
+import { useAutosave } from 'react-autosave'
+
+import { updateEntry } from '@/utils/api'
 
 interface EntryProp {
+  id: string
   content: string
 }
 
 const Editor = ({ entry }: { entry: EntryProp }) => {
   const [value, setValue] = useState(entry.content)
+  const [isLoading, setIsLoading] = useState(false)
+
+  useAutosave({
+    data: value,
+    onSave: async (_value) => {
+      setIsLoading(true)
+      const updated = await updateEntry(entry.id, _value)
+      setIsLoading(false)
+    },
+  })
 
   return (
     <div className="h-full w-full">
+      {isLoading && <div>...Loading</div>}
       <textarea
         className="h-full w-full p-8 text-xl outline-none"
         value={value}
