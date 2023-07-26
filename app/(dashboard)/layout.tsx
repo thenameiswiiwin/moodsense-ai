d75cdfa26e84c907fcc48bb08804d0453948c030
thenameiswiiwin <huyn.nguyen95@gmail.com>
2023-07-26 13:43:38 -0500

    feat((dashboard)/layout.tsx, (dashboard)/journal): added loading state to journal, and nav links

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,10 @@
 import { UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
+
+const links = [
+  { label: 'Home', href: '/' },
+  { label: 'Journal', href: '/journal' },
+]
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -6,6 +12,15 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       <aside className="absolute left-0 top-0 h-full w-[200px] border-r border-black/10">
         <div className="my-4 px-4">
           <span className="text-3xl">Moodsense</span>
+        </div>
+        <div>
+          <ul className="px-4">
+            {links.map((link) => (
+              <li key={link.label} className="px-2 py-6 text-xl">
+                <Link href={link.href}>{link.label}</Link>
+              </li>
+            ))}
+          </ul>
         </div>
       </aside>
       <div className="ml-[200px] h-full w-[calc(100vw-200px)]">
