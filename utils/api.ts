8cca4449247773f27d5b7b39b4d3407bbdd25c93
thenameiswiiwin <huyn.nguyen95@gmail.com>
2023-07-26 19:54:40 -0500

    feat(utils/api.ts): Fetches question answer from API and handles errors

--- utils/api.ts
+++ utils/api.ts
@@ -35,3 +35,19 @@ export const updateEntry = async (id: string, content: string) => {
     )
   }
 }
+
+export const askQuestion = async (question: string) => {
+  const res = await fetch(
+    new Request(createURL('/api/question'), {
+      method: 'POST',
+      body: JSON.stringify({ question }),
+    }),
+  )
+
+  if (res.ok) {
+    const data = await res.json()
+    return data.data
+  } else {
+    throw new Error('Failed to fetch the answer.')
+  }
+}
